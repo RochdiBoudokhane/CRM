03d43b2255a52dfca1a46d8611dbe7f2fd8caafb
MouelhiRawend <47067936+MouelhiRawend@users.noreply.github.com>
2019-11-07 20:54:01 +0100

    Add files via upload

---
+++ Solution.Service/OfferService.cs
@@ -0,0 +1,34 @@
+﻿using Service.Pattern;
+using Solution.Data;
+using Solution.Data.Infrastructure;
+using Solution.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Service
+{
+    public class OfferService : Service<Offer>, IOfferService
+    {
+        private MyContext db = new MyContext();
+        static IDataBaseFactory factory = new DataBaseFactory();
+        static IUnitOfWork UTK = new UnitOfWork(factory);
+        public OfferService() : base(UTK)
+        {
+
+        }
+        public IEnumerable<Offer> GetProductByOffer(int IdOffer)
+        {
+            return GetMany(o => o.OfferId == IdOffer);
+           
+
+        }
+        public List<Product> GetTtireId()
+        {
+            return db.Products.ToList();
+
+        }
+    }
+}
