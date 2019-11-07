c51eae8c2355fa5eeab5a843a81df50424f45907
emna kh <emna.khamessi@esprit.tn>
2019-11-07 14:53:48 +0100

    integration template admin, Crud user

--- Solution.Presentation/Models/UserVm.cs
+++ Solution.Presentation/Models/UserVm.cs
@@ -6,9 +6,13 @@ using System.Web;
 
 namespace Solution.Presentation.Models
 {
-    public class SuperUserCRM
+    public class UserVm
     {
+        [Key]
         public int UserId { get; set; }
+
+
+        public String ImageUrl { get; set; }
 
         [Required(ErrorMessage = "Vous devez indiquer votre prénom")]
         public String FirstName { get; set; }
@@ -17,25 +21,15 @@ namespace Solution.Presentation.Models
         public String LastName { get; set; }
 
 
-        public String Address { get; set; }
+        [Display(Name = "Email")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public String Email { get; set; }
 
-        [MinLength(8)]
-        public int? Phone { get; set; }
-        public int? Cin { get; set; }
-        public int? Points { get; set; }
-        public int? Type { get; set; }
-
-
-        [DataType(DataType.ImageUrl)]
-        public String ImageUrl { get; set; }
-
-
-
-        [Display(Name = "Mot de Passe")]
+        [Display(Name = "Password")]
         [Required, MinLength(6)]
         [DataType(DataType.Password)]
         public String Password { get; set; }
-
 
         [Required]
         [Display(Name = "Confirm password")]
@@ -43,9 +37,16 @@ namespace Solution.Presentation.Models
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
 
-        [Display(Name = "Email")]
-        [Required]
-        [DataType(DataType.EmailAddress)]
-        public String Email { get; set; }
+
+        public int? Type { get; set; }
+        public int? Cin { get; set; }
+        public String Address { get; set; }
+        public int? Phone { get; set; }
+        public int? Points { get; set; }
+
+        public int? CodePostal { get; set; }
+        public int? NbClient { get; set; }
+        public String Document { get; set; }
+        public int? Etat { get; set; }
     }
 }
