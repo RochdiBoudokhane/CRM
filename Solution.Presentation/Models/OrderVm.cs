49255851e54120d0063b2891cdfba360892aa675
omarbbc <57542353+omarbbc@users.noreply.github.com>
2019-11-10 21:36:15 +0100

    Add files via upload

---
+++ Solution.Presentation/Models/OrderVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Solution.Presentation.Models
+{
+    public class OrderVm
+    {
+        [Key, Column(Order = 0)]
+        public int ProductId { get; set; }
+        [Key, Column(Order = 1)]
+        public int CustomerId { get; set; }
+
+        [Key, Column(Order = 2)]
+        [DataType(DataType.Date)]
+        public DateTime Dateres { get; set; }
+        public int Quantity { get; set; }
+    }
+}
